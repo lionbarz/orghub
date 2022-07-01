359340d311bb1ead53665334da721e8c97791cb8
Mohamed Fakhreddine <mohamed.y.fakhreddine@gmail.com>
2022-06-30 17:34:19 -0700

    Add About page.

--- WebService/ClientApp/src/App.js
+++ WebService/ClientApp/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { About } from './components/About';
 import { Groups } from './components/Groups';
 import { Group } from './components/Group';
 
@@ -16,8 +15,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/about' component={About} />
         <Route path='/groups' component={Groups} />
         <Route path='/group/:id' component={Group} />
       </Layout>
---
+++ WebService/ClientApp/src/components/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Mohamed Fakhreddine created Parlilly after repeatedly witnessing motivated and well-intentioned people
+        coming together to form activist groups and then falling out over decisions and governance.</p>
+        <p>The mission of Parlilly is to unite people around the world using the same procedures used to run orderly and
+        deliberative assemblies, from your local Rotary Club to the United States Congress.</p>
+        <p>Give it a try now!</p>
+      </div>
+    );
+  }
+}
