c7266eaf7c4ad1a037c9c83a81cf1b05a7c42c62
Mohamed Fakhreddine <mohamed.y.fakhreddine@gmail.com>
2022-11-17 21:28:15 -0800

    Updates to homepage UI.

--- Core/Motions/IMainMotion.cs
+++ Core/Motions/IMainMotion.cs
@@ -3,7 +3,8 @@ namespace Core.Motions
     /// <summary>
     /// As defined by Robert's Rules of Order, these are Main Motions.
     /// This class is to differentiate main motions from others by
-    /// implementing this interface.
+    /// implementing this interface. Main motions can be moved when
+    /// there is no other motion being considered.
     /// </summary>
     public interface IMainMotion : IMotion
     {
--- WebService/ClientApp/src/components/Home.js
+++ WebService/ClientApp/src/components/Home.js
@@ -47,9 +47,47 @@ export class Home extends Component {
     render() {
         return (
             <div>
-                <div className="jumbotron">
-                    <h1 className="display-4">Organize. Raise money. Grow. Make a difference.</h1>
-                    <p className="lead">Free for groups under 10 members.</p>
+                <div className="px-4 py-5 my-5 text-center">
+                        <h1 className="display-5 fw-bold">Democracy without complexity</h1>
+                        <div className="col-lg-6 mx-auto">
+                            <p className="lead mb-4">Organize, raise money, grow, and make a difference with
+                                Parlilly, a quick and easy online service, featuring elections, voting,
+                                meeting minutes, agendas, and much more.</p>
+                            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                                <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Try it now
+                                </button>
+                                <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary
+                                </button>
+                            </div>
+                        </div>
+                </div>
+                <div className="row align-items-md-stretch">
+                <div className="col-md-4">
+                    <div className="h-100 p-5 bg-light border rounded-3">
+                        <h2>Simplicity</h2>
+                        <p>Bylaws. Parliamentary procedures. These are intimidating words. Luckily, you don't
+                            need to know any of them.
+                            By using Parlilly, your group will leverage
+                            timeless methods used by the most effective organizations,
+                            without you knowing it.</p>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="h-100 p-5 bg-light border rounded-3">
+                        <h2>Democracy</h2>
+                        <p>Successful clubs have active members. Parlilly makes it easy for all members
+                            to participate. Make suggestions, vote, debate, and much more, all with a press
+                            of a button.</p>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="h-100 p-5 bg-light border rounded-3">
+                        <h2>Transparency</h2>
+                        <p>Knowledge is power, and transparency empowers your members.
+                            Parlilly automatically records basic meeting minutes and makes it easy to see
+                            the group's officers, members, agenda, decisions, and more.</p>
+                    </div>
+                </div>
                 </div>
                 <h2>Who are you?</h2>
                 {this.state.user && <p>Hello, {this.state.user.name}</p>}
